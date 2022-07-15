c50477a553d5831c850e23868dbeb07f5d9a7873
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2022-07-15 12:27:58 +0800

    Remove irrelevant files

--- cuser.js
+++
@@ -1,7 +0,0 @@
-const { UserManager } = require('./server/user/manage-user');
-const um = new UserManager();
-um.createUser_CLI('GW_MC', 'password');
-um.createUser_CLI('GW_MC', 'password', 60);
-um.createUser_CLI('GW_MC', 'password', 60);
-um.createUser_CLI('GW_MC', 'password', 50);
-um.logUserList();
