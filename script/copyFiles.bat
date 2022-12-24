ce4e5ae9c3d36122c45087e1f576ea03511fa72e
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2022-12-24 13:04:04 +0800

    script copy js library to bin from src

--- script/copyFiles.bat
+++ script/copyFiles.bat
@@ -1,6 +1,9 @@
 @ECHO off
-set flag=/s/e/v/x/k/y/d
+set flag=/s/e/v/x/k/y/d/f
+@REM Do NOT remove /f flag, it fixes false cyclic copy error for js part
 ECHO Copying HTML/Pug files && ^
 xcopy %flag% "%cd%/src/client/html" "%cd%/bin/client/html" && ^
 ECHO Copying CSS files && ^
 xcopy %flag% "%cd%/src/client/static/css" "%cd%/bin/client/static/css" && ^
+ECHO Copying Non-compiled JS files && ^
+xcopy %flag% "%cd%/src/client/static/javascript/*.js" "%cd%/bin/client/static/javascript"
