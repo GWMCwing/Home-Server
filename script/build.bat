bb5a30a3218783dfac4a3eb97331a10e7b97e7d2
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2022-12-28 18:42:27 +0800

    fixed server side common from compiling into module

--- script/build.bat
+++ script/build.bat
@@ -1,5 +1,6 @@
 @ECHO off
-ECHO Compiling Server side Typescript && ^
-npx tsc --project tsconfig.server.json && ECHO Compile Complete && ^
+@REM Compile client side before compiling server side to prevent server side compiling into module
 ECHO Compiling Client side Typescript && ^
-npx tsc --project tsconfig.client.json && ECHO Compile Complete
+npx tsc --project tsconfig.client.json && ECHO Compile Complete ^
+ECHO Compiling Server side Typescript && ^
+npx tsc --project tsconfig.server.json && ECHO Compile Complete
