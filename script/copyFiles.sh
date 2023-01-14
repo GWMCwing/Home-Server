0303f40ced82a85b6c9280524b7430098ab2174b
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2023-01-14 13:41:27 +0000

    update for linux support

--- script/copyFiles.sh
+++ script/copyFiles.sh
@@ -2,8 +2,9 @@
 
 # Do NOT remove /f flag, it fixes false cyclic copy error for js part
 echo Copying HTML/Pug Files to bin && \
-cp --update "${PWD}/src/client/html" "${PWD}/bin/client/html" && \
+cp -r --update "${PWD}/src/client/html" "${PWD}/bin/client/html" && \
 echo Copying CSS Files to bin && \
-cp --update "${PWD}/src/client/static/css" "${PWD}/bin/client/static/css" && \
+cp -r --update "${PWD}/src/client/static/css" "${PWD}/bin/client/static/css" && \
 echo Copying Non-compiled JS Files to bin && \
-cp --update "${PWD}/src/client/static/javascript/*.js" "${PWD}/bin/client/static/javascript"
+# cp -r --update "${PWD}/src/client/static/javascript/**/*.js" "${PWD}/bin/client/static/javascript"
+cp -r --update $(find ${PWD}/src/client/static/javascript -name "*.js") "${PWD}/bin/client/static/javascript"
