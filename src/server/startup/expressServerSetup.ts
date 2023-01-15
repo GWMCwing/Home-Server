44ee405c2065276ab1e7d0f4b522f6aa0994cdac
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2023-01-15 02:41:57 +0000

    https support

--- src/server/startup/expressServerSetup.ts
+++ src/server/startup/expressServerSetup.ts
@@ -6,9 +6,12 @@ import { initiateDatabaseInterface } from '../database/databaseInterface';
 import { serveStatic } from '../middleware/static/serveStatic';
 import { setupRouter } from '../router/rootRouter';
 import { loadErrorHandlerMiddleware } from '../middleware/errorHandlerMiddleware';
-
+//
 import { Express } from 'express';
+import { createServer as createServer_http } from 'http';
+import { createServer as createServer_https } from 'https';
 import { MongoClient } from 'mongodb';
+import { readFileSync } from 'fs';
 //
 const threadName = 'Main';
 //
@@ -32,6 +35,7 @@ export function startup_expressServer(
     isInDev: boolean
 ) {
     //
+    // console.log('setting');
     setupLogging(app, isInDev);
     setupRendering(app);
 
@@ -41,15 +45,28 @@ export function startup_expressServer(
     // serving static files and
     // prevent webpage from showing in search engine
     loadPreMiddleware(app, db);
+    // console.log('setting');
     //
     initiateDatabaseInterface(db, 'Express' + (isInDev ? '_dev' : ''));
     //
     serveStatic(app);
     //
+    // console.log('setting');
     setupRouter(app);
     //
     loadErrorHandlerMiddleware(app);
-    app.listen(port, function () {
-        CLL.log(threadName, 'Express Startup', `Running on port ${port}`);
-    });
+
+    // app.listen(port, function () {
+    //     CLL.log(threadName, 'Express Startup', `Running on port ${port}`);
+    // });
+    CLL.log(threadName, 'Express Startup', 'Loading key');
+    const https_options = {
+        key: readFileSync(process.env.HTTPS_KEY as string),
+        cert: readFileSync(process.env.HTTPS_CERT as string),
+        ca: readFileSync(process.env.HTTPS_CA as string),
+    };
+    createServer_http(app).listen(port);
+    CLL.log(threadName, 'Express Startup', `Running http on port ${port}`);
+    createServer_https(https_options, app).listen(443);
+    CLL.log(threadName, 'Express Startup', `Running https on port 443`);
 }
